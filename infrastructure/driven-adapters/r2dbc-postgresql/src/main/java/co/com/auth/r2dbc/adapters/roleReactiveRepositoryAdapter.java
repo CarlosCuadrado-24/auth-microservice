874a098de4439e2b009accba5a114abfec8adc8e
CarlosCuadrado-24 <carloscuadradot24@gmail.com>
2025-08-25 13:29:33 -0500

    agregando logs

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/auth/r2dbc/adapters/roleReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/auth/r2dbc/adapters/roleReactiveRepositoryAdapter.java
@@ -5,11 +5,13 @@ import co.com.auth.model.role.gateways.RoleRepository;
 import co.com.auth.r2dbc.entities.RoleEntity;
 import co.com.auth.r2dbc.helper.ReactiveAdapterOperations;
 import co.com.auth.r2dbc.repositories.IRoleReactiveRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Repository
 public class roleReactiveRepositoryAdapter extends ReactiveAdapterOperations<
         Role,
@@ -24,13 +26,21 @@ public class roleReactiveRepositoryAdapter extends ReactiveAdapterOperations<
     @Transactional
     @Override
     public Mono<Role> saveRole(Role role) {
+        log.info("Iniciando guardado del rol: {}", role);
         return repository.save(mapper.map(role,RoleEntity.class))
+                .doOnSubscribe(sub -> log.debug("Guardando entidad en base de datos: {}", role))
+                .doOnSuccess(saved -> log.info("rol guardado exitosamente con ID: {}", saved.getId()))
+                .doOnError(error -> log.error("Error al guardar el rol: {}", error.getMessage(), error))
                 .map(saved->mapper.map(saved, Role.class));
     }
 
     @Transactional
     @Override
     public Mono<Role> getRoleById(Long id) {
-        return repository.findById(id).map(roleEntity->mapper.map(roleEntity, Role.class));
+        log.info("Iniciando busqueda por id: {}", id);
+        return repository.findById(id)
+                .doOnSuccess(saved -> log.info("rol encontrado exitosamente con ID: {}", saved))
+                .doOnError(error -> log.error("Error al ecnontrar el rol: {}", error.getMessage(), error))
+                .map(roleEntity->mapper.map(roleEntity, Role.class));
     }
 }
--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/auth/r2dbc/adapters/userReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/auth/r2dbc/adapters/userReactiveRepositoryAdapter.java
@@ -5,11 +5,13 @@ import co.com.auth.model.user.gateways.UserRepository;
 import co.com.auth.r2dbc.entities.UserEntity;
 import co.com.auth.r2dbc.helper.ReactiveAdapterOperations;
 import co.com.auth.r2dbc.repositories.IUserReactiveRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Repository
 public class userReactiveRepositoryAdapter extends ReactiveAdapterOperations<
     User, UserEntity,
@@ -23,11 +25,14 @@ public class userReactiveRepositoryAdapter extends ReactiveAdapterOperations<
     @Transactional
     @Override
     public Mono<User> saveUser(User user) {
+        log.info("Iniciando guardado de usuario: {}", user);
         UserEntity entity = mapper.map(user, UserEntity.class);
-
         entity.setRoleId(user.getRole() != null ? user.getRole().getId() : null);
 
         return repository.save(entity)
+                .doOnSubscribe(sub -> log.debug("Guardando entidad en base de datos: {}", entity))
+                .doOnSuccess(saved -> log.info("Usuario guardado exitosamente con ID: {}", saved.getId()))
+                .doOnError(error -> log.error("Error al guardar el usuario: {}", error.getMessage(), error))
                 .map(saved -> {
                     User out = mapper.map(saved, User.class);
                     out.setRole(user.getRole());
